1176fc73f626340f5cc8acbca07957baa73a6cfa
Si Mills <si@sjm-create.co.uk>
2023-09-26 14:03:48 +0100

    Adds nfd extended, compiles from csd path

--- CsoundTest.cpp
+++ CsoundTest.cpp
@@ -1,13 +1,13 @@
-
 #include "CsoundTest.h"
-#include "CsoundLib64/csound.hpp"
 #include <iostream>
 #include <string>
-#include <array>
 #include <math.h>
 
+//#include "nfd.hpp"
+
 #ifdef __APPLE__
-#include <corefoundation/CFBundle.h>    
+//#include <corefoundation/CFBundle.h>
+#include <CoreFoundation/CoreFoundation.h>
 #endif
 
 using namespace plogue::biduleSDK;
@@ -29,68 +29,109 @@ BidulePlugin(host){
 	_numMagIns=0;
 	_numMagOuts=0;
     _numUIColumns=1;
-    _numParams = 2;
+    _numParams = 5;
     
+    // own params
     _blurAmt = 0.25;
     
-//    for (int i = 0; i < 256; i++) {
-//        sinTable[i] = (float)i/256;
-//        cout << sinTable[i] << endl;
-//    }
-//    cout << "sinTable size: " << sinTable.size() << endl;
-//    cout << M_PI << endl;
- 
+    _csCompileResult = -1;
+    _ksmpsIndex = 0;
+    _triggerOpenDialog = 0;
 }
 
 CsoundTest::~CsoundTest(){
-    //free Csound object
-    delete csound;
+    //free Csound stuff
+    _csound->Cleanup();
+    _csound->Reset();
+//    if (_inputFile.is_open()) {
+//        _inputFile.close();
+//    }
 }
 
-bool CsoundTest::init() {
-    cout << "CsoundTest init" << endl;
-    
-    // get the local bundle CSD from Resources...
-    CFBundleRef mainBundle = CFBundleGetBundleWithIdentifier(CFSTR("com.boonier.CsoundTest"));
-    CFURLRef csdURL = CFBundleCopyResourceURL(mainBundle, CFSTR("test8.csd"), NULL, NULL);
-    string path = CFStringGetCStringPtr(CFURLGetString(csdURL), kCFStringEncodingMacRoman );
-    string sub = "file://";
-    string::size_type i = path.find(sub);
-    if (i != string::npos)
-        path.erase(i, sub.length());
-    
-    cout << "Init CsoundTest" << endl;
-    csound = new Csound();
-    cout << csound << endl;
-    cout << "version:" << csound->GetVersion() << endl;
-    cout << "api version:" << csound->GetAPIVersion() << endl;
-    csound->CreateMessageBuffer(0);
-   
-    //compile instance of csound.
-    csCompileResult = csound->Compile(path.c_str());
-    csound->SetHostImplementedAudioIO(1, 0);
-    
-    //setup performance thread
-//    CsoundPerformanceThread* perfThread = new CsoundPerformanceThread(csound);
-    
-    if (csCompileResult == 0) { // compiled OK...
-        cout << "CSOUND_MESSAGE:" << csound->GetFirstMessage() << endl;
-        //access Csound's input/output buffer
-        spout = csound->GetSpout();
-        spin  = csound->GetSpin();
-        
-        //start Csound performance
-        csound->Start();
-        cout << "Successful CSD compile, starting..." << endl;
-        return true;
-    } else {
-        while (csound->GetMessageCnt() > 0) {
-           cout << "CSOUND_MESSAGE:" << csound->GetFirstMessage() << endl;
-           csound->PopFirstMessage();
+void
+CsoundTest::log(const std::string& message) {
+    cout << "[LOG] " << message << endl;
+}
+
+void
+CsoundTest::openCsdFile() {
+    if (_triggerOpenDialog == 1) {
+        log("openCsdFile" + to_string(_triggerOpenDialog));
+        // initialize NFD
+        NFD::Guard nfdGuard;
+        // auto-freeing memory
+        NFD::UniquePath outPath;
+        // only allow .csd files
+        nfdfilteritem_t filterItem[2] = {{"Csound CSD file", "csd"}};
+
+        // show the dialog
+        nfdresult_t result = NFD::OpenDialog(outPath, filterItem, 1);
+        if (result == NFD_OKAY) {
+            string outPathStr(outPath.get());
+            log("outPath result = " + outPathStr);
+            // store outPathStr to label
+            _savedCsdPath = outPathStr;
+            updateParameter(3, _savedCsdPath);
+            
+            // update the gui
+//            size_t lastSlashPos = outPathStr.find_last_of("/");
+//            _displayedCsdPath = outPathStr.substr(lastSlashPos + 1);
+//            updateParameter(4, _displayedCsdPath);
+            setDisplayLabel(outPathStr);
+            
+        } else if (result == NFD_CANCEL) {
+            log("User pressed cancel.");
+        } else {
+            std::cout << "Error: " << NFD::GetError() << std::endl;
         }
-        cout << "CSD did not compile:" << csCompileResult << endl;
-        return false;
+        
+        // reset trigger state
+        _triggerOpenDialog = 0;
     }
+
+}
+
+bool
+CsoundTest::init() {
+    log("CsoundTest init");
+    
+    return true;
+    
+    
+//    _csound = make_unique<Csound>(new Csound());
+//    cout << _csound << endl;
+//    cout << "version:" << _csound->GetVersion() << endl;
+//    cout << "api version:" << _csound->GetAPIVersion() << endl;
+//    _csound->CreateMessageBuffer(0);
+//    _csound->SetHostImplementedAudioIO(1, 0);
+//
+//
+//    if (_filePath.length()) {
+//        _csCompileResult = _csound->Compile(_filePath.c_str());
+//        log("_csCompileResult = " + to_string(_csCompileResult));
+//
+//        if (_csCompileResult == 0) { // compiled OK...
+//            cout << "CSOUND_MESSAGE:" << _csound->GetFirstMessage() << endl;
+//            //access Csound's input/output buffer
+//            spout = _csound->GetSpout();
+//            spin  = _csound->GetSpin();
+//
+//            //start Csound performance
+//            _csound->Start();
+//            cout << "Successful CSD compile, starting..." << endl;
+//            return true;
+//        } else {
+//            while (_csound->GetMessageCnt() > 0) {
+//               cout << "CSOUND_MESSAGE:" << _csound->GetFirstMessage() << endl;
+//               _csound->PopFirstMessage();
+//            }
+//            cout << "CSD did not compile:" << _csCompileResult << endl;
+//            return false;
+//        }
+//    } else {
+//        std::cerr << "Failed to read the file!" << std::endl;
+//        return false;
+//    }
     
 }
 
@@ -149,9 +190,7 @@ CsoundTest::getParametersInfos(ParameterInfo* pinfos) {
     pinfos[0].paramInfo.pd.minValue = 0;
     pinfos[0].paramInfo.pd.maxValue = 20;
     pinfos[0].paramInfo.pd.precision = 3;
-    //    strcpy(pinfos[0].paramInfo.pd.units, "dollar");
-//    pinfos[0].paramInfo.pd.scaling = SCALING_LINEAR;
-    
+
     pinfos[1].id = 1;
     strcpy(pinfos[1].name, "pitch");
     pinfos[1].type = DOUBLEPARAM;
@@ -162,10 +201,96 @@ CsoundTest::getParametersInfos(ParameterInfo* pinfos) {
     pinfos[1].paramInfo.pd.minValue = 60;
     pinfos[1].paramInfo.pd.maxValue = 800;
     pinfos[1].paramInfo.pd.precision = 3;
+    
+    pinfos[2].id = 2;
+    strcpy(pinfos[2].name, "Choose CSD file");
+    pinfos[2].type = BOOLPARAM;
+    pinfos[2].ctrlType = GUICTRL_BUTTON;
+    pinfos[2].linkable = 1;
+    pinfos[2].saveable = 0;
+    pinfos[2].paramInfo.pb.isTrigger = 1;
+    pinfos[2].paramInfo.pb.defaultValue = 0;
+    
+    // state
+    pinfos[3].id = 3;
+    strcpy(pinfos[3].name, "");
+    pinfos[3].type = STRINGPARAM;
+    pinfos[3].ctrlType = GUICTRL_TEXTBOX;
+    pinfos[3].linkable = 0;
+    pinfos[3].saveable = 1;
+    strcpy(pinfos[3].paramInfo.ps.defaultValue, "");
+    
+    pinfos[4].id = 4;
+    strcpy(pinfos[4].name, "csd name:");
+    pinfos[4].type = STRINGPARAM;
+    pinfos[4].ctrlType = GUICTRL_LABEL;
+    pinfos[4].linkable = 0;
+    pinfos[4].saveable = 0;
+    strcpy(pinfos[4].paramInfo.ps.defaultValue, "No path has been set");
+
 }
 
 void 
 CsoundTest::getParameterChoices(long id, std::vector<std::string>& vec) {
+}
+
+void
+CsoundTest::setDisplayLabel(string& label) {
+    size_t lastSlashPos = label.find_last_of("/");
+    _displayedCsdPath = label.substr(lastSlashPos + 1);
+    updateParameter(4, _displayedCsdPath);
+}
+
+void
+CsoundTest::compileCsdFile(){
+    log("calling compileCsdFile");
+    // 1. On init, check _savedCsdPath for a stored csd path
+    //      a. if nothing found, do nothing
+    // 2. If path found, check for existing instance of Csound
+    //      a. if Csound found, assign value of nullptr
+    // 3. then create new Csound instance
+    // 4. call csound->Compile(...), and store result to _csCompileResult
+    // 5.
+    
+    if (_csound.get() != nullptr) {
+        _csound = nullptr;
+    }
+    
+    _csound = make_unique<Csound>(new Csound());
+    cout << _csound << endl;
+    cout << "version:" << _csound->GetVersion() << endl;
+    cout << "api version:" << _csound->GetAPIVersion() << endl;
+    _csound->CreateMessageBuffer(0);
+    _csound->SetHostImplementedAudioIO(1, 0);
+    
+    
+    if (_savedCsdPath.length() > 0) {
+        _csCompileResult = _csound->Compile(_savedCsdPath.c_str());
+        log("_csCompileResult = " + to_string(_csCompileResult));
+                
+        if (_csCompileResult == 0) { // compiled OK...
+            cout << "CSOUND_MESSAGE:" << _csound->GetFirstMessage() << endl;
+            //access Csound's input/output buffer
+            spout = _csound->GetSpout();
+            spin  = _csound->GetSpin();
+
+            //start Csound performance
+            _csound->Start();
+            cout << "Successful CSD compile, starting..." << endl;
+//            return true;
+        } else {
+            while (_csound->GetMessageCnt() > 0) {
+               cout << "CSOUND_MESSAGE:" << _csound->GetFirstMessage() << endl;
+               _csound->PopFirstMessage();
+            }
+            cout << "CSD did not compile:" << _csCompileResult << endl;
+//            return false;
+        }
+    } else {
+        std::cerr << "Failed to read the file!" << std::endl;
+//        return false;
+    }
+
 }
 
 void 
@@ -174,7 +299,25 @@ CsoundTest::parameterUpdate(long id) {
         getParameterValue(0, _blurAmt);
     } else if (id == 1) {
         getParameterValue(1, _pitch);
+    } else if (id == 2) {
+        getParameterValue(2, _triggerOpenDialog);
+        openCsdFile();
+    } else if (id == 3) {
+        getParameterValue(3, _savedCsdPath);
+        log("parameterUpdate _savedCsdPath = " + _savedCsdPath);
+        
+        if (_savedCsdPath.length()) {
+            // update the ez label
+            setDisplayLabel(_savedCsdPath);
+            //compile the csd as we have a path
+            compileCsdFile();
+        }
     }
+//        else if (id == 4) {
+//
+//        log("parameterUpdate _storedCsdPath = " + _savedCsdPath);
+//        // call re-render here (if there is a valid stored path)
+//    }
 }
 void 
 CsoundTest::process(Sample** sampleIn, Sample** sampleOut, MIDIEvents* midiIn, MIDIEvents* midiOut, Frequency*** freqIn, Frequency*** freqOut, Magnitude*** magIn, Magnitude*** magOut, SyncInfo* syncIn, SyncInfo* syncOut){
@@ -192,29 +335,25 @@ CsoundTest::process(Sample** sampleIn, Sample** sampleOut, MIDIEvents* midiIn, M
 //           }
 //        }
         
-        if (csCompileResult == 0) {
-            if (ksmpsIndex == csound->GetKsmps()) {
-                csCompileResult = csound->PerformKsmps();
-                if (csCompileResult == 0)
-                    ksmpsIndex = 0;
+        if (_csCompileResult == 0) {
+            if (_ksmpsIndex == _csound->GetKsmps()) {
+                _csCompileResult = _csound->PerformKsmps();
+                if (_csCompileResult == 0)
+                    _ksmpsIndex = 0;
             }
             
-            csound->SetChannel("phsRate", _blurAmt);
-            csound->SetChannel("pitch", _pitch);
+            _csound->SetChannel("phsRate", _blurAmt);
+            _csound->SetChannel("pitch", _pitch);
             
-            (*s1++) = spout[0 + (ksmpsIndex * channels)];
-            (*s2++) = spout[1 + (ksmpsIndex * channels)];
-            ksmpsIndex++;
+            (*s1++) = spout[0 + (_ksmpsIndex * channels)];
+            (*s2++) = spout[1 + (_ksmpsIndex * channels)];
+            
+            _ksmpsIndex++;
         } else {
             (*s1++) = 0.f;
             (*s2++) = 0.f;
         }
-        
-//        (*s1++) = sinTable[position];
-//        (*s2++) = sinTable[position];
-//        position = position < sinTable.size() ? position+1 : 0;
     }
 	
-
 }
 
--- CsoundTest.h
+++ CsoundTest.h
@@ -3,9 +3,14 @@
 
 #include "../common/BiduleSDK.h"
 #include <string>
+#include <memory>
+#include <fstream>
 #include "CsoundLib64/csound.hpp"
-//#include "CsoundLib64/csPerfThread.hpp"
-#include <array>
+//#include "/Users/boonier/GIT/nativefiledialog-extended/src/include/nfd.hpp"
+
+//extern "C" {
+    #include "nfd.hpp"
+//}
 
 using namespace std;
 
@@ -29,20 +34,22 @@ namespace acme {
             virtual void parameterUpdate(long id);
 			virtual void getParametersInfos(ParameterInfo* pinfos);
 			virtual void getParameterChoices(long id, vector<string>& vec);
+//            virtual void updateParameter(long id, const std::string& strVal);
 
 			virtual void process(Sample** sampleIn, Sample** sampleOut, MIDIEvents* midiIn, MIDIEvents* midiOut, Frequency*** freqIn, Frequency*** freqOut, Magnitude*** magIn, Magnitude*** magOut, SyncInfo* syncIn, SyncInfo* syncOut);
 		
         protected:
-            //Create an instance of Csound
-            Csound* csound;
-            string csoundText;
-            MYFLT *spin, *spout;
-            int csCompileResult = -1;
-            int ksmpsIndex = 0;
-            array<float, 256> sinTable;
-            double _blurAmt;
-        double _pitch;
-            
+        void log(const string& message);
+        void openCsdFile();
+        void compileCsdFile();
+        void setDisplayLabel(string& label) ;
+        
+        string _savedCsdPath, _displayedCsdPath;
+        string _filePath;
+        unique_ptr<Csound> _csound;
+        MYFLT *spin, *spout;
+        int _csCompileResult, _ksmpsIndex, _triggerOpenDialog;
+        double _blurAmt, _pitch;
     };
 };
 
